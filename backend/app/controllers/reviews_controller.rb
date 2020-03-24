53382d903c59073fb48b15c725f6470ce4e37cf4
abidtalukdar <abiditalukdar@gmail.com>
2020-03-24 18:09:41 -0400

    4th Commit - CRD Functions

--- backend/app/controllers/reviews_controller.rb
+++ backend/app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+    def index 
+        @reviews = Review.all
+        render json: @reviews
+    end
+
+    def show
+        @review = Review.find_by(id: params[:id])
+        render json: @review
+    end
+
+    def create 
+        Review.create(review_params)
+    end
+    
+    def destroy
+        @review = Review.find_by(id: params[:id])
+        @review.destroy
+    end
+
+        private 
+        
+        def review_params
+            params.permit(:movie_id, :rating, :review, :reviewer)
+        end
 end
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,10 @@ const renderAllMovies = (moviesData) => {
     moviesData.forEach(renderOneMovie)
 }
 
+
+
+
+
 // renders one movie at a time and after the movie is appended to the movieContainer on line 31, the reviews for each movie are added throught the renderReview function on line 34
 const renderOneMovie = (movie) => {
     const movieDiv = document.createElement("div")
@@ -24,20 +28,89 @@ const renderOneMovie = (movie) => {
         <h3>Synopsis:</h3>
         ${movie.synopsis}
         <h3>Movie Reviews:</h3> 
+        <form id="${movie.id}-review-form" data-id="${movie.id}">
+            <label>Movie Rating:</label>
+            <input type="number" name="rating" min="0" max="10" step="0.5"><br>
+
+            <label>Please Enter Your Review:</label>
+            <input type="text" name="review" placeholder="Add Review"><br>
+
+            <label>Please Enter Your Name:</label>
+            <input type="text" name="reviewer" placeholder="Name"><br>
+
+            <input type="submit" value="Post Your Review">
+        </form>
         <h4>Average Audience Rating: <i>${movie.movie_rating}</i></h4>
         <ul id="${movie.id}" class="reviews-container">
         </ul>
     `
-    movieContainer.append(movieDiv)
+
+    movieContainer.append(movieDiv) // movie is on index.html
     const reviewUl = document.getElementById(`${movie.id}`) // each movie has a reviews container which can be accessed by using document.getElementById(id). "id" being the movie's id
     movie.reviews.forEach(review => {
         return renderReview(review, reviewUl)
     })
 
+    const reviewForm = document.getElementById(`${movie.id}-review-form`)
+    reviewForm.addEventListener("submit", event => {
+        handleReviewFormSubmit(event)
+    })
 }
+
+
+
 // this function renders all the comments by taking in one review hash and the reviewsUl for that specific movie
 const renderReview = (review, reviewUl) => {
     const reviewLi = document.createElement("li")
-    reviewLi.innerText = `"${review.rating}/10 ${review.review}" - ${review.reviewer}`
+    reviewLi.dataset.id = review.id
+    reviewLi.className = "review-post"
+    reviewLi.innerHTML = `
+    "${review.rating}/10 ${review.review}" - ${review.reviewer} <button data-action="delete-review" class="delete">x</button>
+    `
     reviewUl.append(reviewLi)
+
+    // const reviewPost = document.querySelector(`[data-id="${review.id}"]`)
+    reviewLi.addEventListener("click", event => {
+        if (event.target.dataset.action === "delete-review") {
+            fetch(`http://localhost:3000/reviews/${review.id}`, {
+                method: "DELETE"
+            })
+            .then(response => {
+                console.log(response)
+                return response.json()
+              })
+            .then(() => {
+                reviewLi.remove()
+            })
+        }
+    })
 }
+
+
+
+const handleReviewFormSubmit = (event) => {
+    event.preventDefault()
+    const form = event.target
+    const reviewUl = document.getElementById(`${form.dataset.id}`)
+    console.log(reviewUl)
+    const newReview = {
+        movie_id: form.dataset.id,
+        rating: form.rating.value,
+        review: form.review.value,
+        reviewer: form.reviewer.value
+    }
+
+    renderReview(newReview, reviewUl)
+
+    fetch(`http://localhost:3000/reviews`, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newReview)
+    })
+    .then(response => response.json())
+}
+
+
